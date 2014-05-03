3df3c850133b71f328790c7c0eeb21ecd7609387
Shouko <h999342@gmail.com>
2014-05-03 20:25:11 +0800

    fix missing semi-colon in fbimg.php

--- fbimg.php
+++ fbimg.php
@@ -1,6 +1,6 @@
 <?php
 if(isset($_GET['u'])){
 	$fbid = abs($_GET['u']);
-	echo file_get_contents('http://graph.facebook.com/'.$fbid.'/picture?width=150')
+	echo file_get_contents('http://graph.facebook.com/'.$fbid.'/picture?width=150');
 }
 ?>
