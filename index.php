2f5e1b20b24b365bc1ac6e4f80cba75d6444ad4d
shouko <imshouko@gmail.com>
2016-09-15 15:46:35 +0800

    add ga

--- index.php
+++ index.php
@@ -246,5 +246,15 @@ function uploadToIu(){
   </div>
 
   <div id="footer">by Shouko, NTUIM</div>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-45786123-5', 'auto');
+  ga('send', 'pageview');
+
+</script>
 </body>
 </html>
