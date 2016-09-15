516487de8f5a8b27441855434a9df071eb4efc46
Shouko <h999342@gmail.com>
2016-09-15 06:58:59 +0000

    fix https

--- index.php
+++ index.php
@@ -17,11 +17,11 @@
 
 	<title>NTU ID Creator</title>
 	<meta charset="UTF-8">
-	<link rel="stylesheet" href="style.css?<?php echo time()?>">
-	<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
   <script src="./html2canvas.js"></script>
-  <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
-  <script src="http://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+  <script src="//code.jquery.com/jquery-1.9.1.js"></script>
+  <script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
   <script type="text/javascript">
 
 function postToWall() {  
